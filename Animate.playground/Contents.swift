a51fda8f2eff2d8180389d91453415f03e7143ec
John Sundell <john@sundell.co>
2017-07-29 16:49:48 +0200

    Update for blog post # 2

--- Animate.playground/Contents.swift
+++ Animate.playground/Contents.swift
@@ -16,18 +16,109 @@ public struct Animation {
 
 public extension Animation {
     static func fadeIn(duration: TimeInterval = 0.3) -> Animation {
-        return Animation(duration: duration, closure: { $0.alpha = 1 })
+        return Animation(duration: duration) { $0.alpha = 1 }
+    }
+
+    static func fadeOut(duration: TimeInterval = 0.3) -> Animation {
+        return Animation(duration: duration) { $0.alpha = 0 }
     }
 
     static func resize(to size: CGSize, duration: TimeInterval = 0.3) -> Animation {
-        return Animation(duration: duration, closure: { $0.bounds.size = size })
+        return Animation(duration: duration) { $0.bounds.size = size }
+    }
+
+    static func move(byX x: CGFloat, y: CGFloat, duration: TimeInterval = 0.3) -> Animation {
+        return Animation(duration: duration) {
+            $0.center.x += x
+            $0.center.y += y
+        }
     }
 }
 
-public extension UIView {
-    func animate(_ animations: [Animation]) {
-        guard !animations.isEmpty else {
+public final class AnimationToken {
+    private let view: UIView
+    private let animations: [Animation]
+    private let mode: AnimationMode
+    private var isValid = true
+
+    internal init(view: UIView, animations: [Animation], mode: AnimationMode) {
+        self.view = view
+        self.animations = animations
+        self.mode = mode
+    }
+
+    deinit {
+        perform {}
+    }
+
+    internal func perform(completionHandler: @escaping () -> Void) {
+        guard isValid else {
             return
+        }
+
+        isValid = false
+
+        switch mode {
+        case .inSequence:
+            view.performAnimations(animations, completionHandler: completionHandler)
+        case .inParallel:
+            view.performAnimationsInParallel(animations, completionHandler: completionHandler)
+        }
+    }
+}
+
+internal enum AnimationMode {
+    case inSequence
+    case inParallel
+}
+
+public func animate(_ tokens: [AnimationToken]) {
+    guard !tokens.isEmpty else {
+        return
+    }
+
+    var tokens = tokens
+    let token = tokens.removeFirst()
+
+    token.perform {
+        animate(tokens)
+    }
+}
+
+public func animate(_ tokens: AnimationToken...) {
+    animate(tokens)
+}
+
+public extension UIView {
+    @discardableResult func animate(_ animations: [Animation]) -> AnimationToken {
+        return AnimationToken(
+            view: self,
+            animations: animations,
+            mode: .inSequence
+        )
+    }
+
+    @discardableResult func animate(_ animations: Animation...) -> AnimationToken {
+        return animate(animations)
+    }
+
+    @discardableResult func animate(inParallel animations: [Animation]) -> AnimationToken {
+        return AnimationToken(
+            view: self,
+            animations: animations,
+            mode: .inParallel
+        )
+    }
+
+    @discardableResult func animate(inParallel animations: Animation...) -> AnimationToken {
+        return animate(inParallel: animations)
+    }
+}
+
+internal extension UIView {
+    func performAnimations(_ animations: [Animation], completionHandler: @escaping () -> Void) {
+        guard !animations.isEmpty else {
+            return completionHandler()
         }
 
         var animations = animations
@@ -36,15 +127,32 @@ public extension UIView {
         UIView.animate(withDuration: animation.duration, animations: {
             animation.closure(self)
         }, completion: { _ in
-            self.animate(animations)
+            self.performAnimations(animations, completionHandler: completionHandler)
         })
     }
 
-    func animate(inParallel animations: [Animation]) {
-        for animation in animations {
-            UIView.animate(withDuration: animation.duration) {
-                animation.closure(self)
+    func performAnimationsInParallel(_ animations: [Animation], completionHandler: @escaping () -> Void) {
+        guard !animations.isEmpty else {
+            return completionHandler()
+        }
+
+        let animationCount = animations.count
+        var completionCount = 0
+
+        let animationCompletionHandler = {
+            completionCount += 1
+
+            if completionCount == animationCount {
+                completionHandler()
             }
+        }
+
+        for animation in animations {
+            UIView.animate(withDuration: animation.duration, animations: {
+                animation.closure(self)
+            }, completion: { _ in
+                animationCompletionHandler()
+            })
         }
     }
 }
@@ -60,18 +168,29 @@ view.backgroundColor = .white
 
 PlaygroundPage.current.liveView = view
 
-// MARK: - Performing the animation
+// MARK: - View setup
 
-let animationView = UIView(frame: CGRect(
-    x: 0, y: 0,
-    width: 50, height: 50
-))
+let label = UILabel()
+label.text = "Let's animate..."
+label.sizeToFit()
+label.center = view.center
+label.alpha = 0
+view.addSubview(label)
 
-animationView.backgroundColor = .red
-animationView.alpha = 0
-view.addSubview(animationView)
+let button = UIButton(type: .system)
+button.setTitle("...multiple views!", for: .normal)
+button.sizeToFit()
+button.center.x = view.center.x
+button.center.y = label.frame.maxY + 50
+button.alpha = 0
+view.addSubview(button)
 
-animationView.animate(inParallel: [
-    .fadeIn(duration: 3),
-    .resize(to: CGSize(width: 200, height: 200), duration: 3)
-])
+// MARK: - Performing animation
+
+animate(
+    label.animate(
+        .fadeIn(duration: 3),
+        .move(byX: 0, y: -50, duration: 3)
+    ),
+    button.animate(.fadeIn(duration: 3))
+)
